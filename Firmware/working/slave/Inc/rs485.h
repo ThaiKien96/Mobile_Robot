cecadbc56dfe2be244b9d06f1b64d99e7bef1574
ThaiKien96 <thaikien.kc@gmail.com>
2018-11-18 14:28:13 +0700

    [rs485] Add new RS485 module for the slave
    [Target] Firmware
    
    [Descriptions]-

--- Firmware/working/slave/Inc/rs485.h
+++ Firmware/working/slave/Inc/rs485.h
@@ -1,64 +1,74 @@
 /******************************************************************************
-* #./Inc/rs485.h
-* Project   :   LVTN
-* Module    :   rs485
-* Author    :   Le Thai Kien
-* Date      :   10/10/2018
-******************************************************************************/
+ * #./Core/Inc/rs485.h
+ * 
+ * Project:     LVTN
+ * Module:      RS485
+ * Author:      Le Thai Kien
+ * Date:        08/10/2018
+ * Description: Module use to comunicate with the slaves that are driver
+ *              control Motor.
+ *****************************************************************************/
 
-#include "stm32f4xx_hal_uart.h"
+#ifndef __RS485_H__
+#define __RS485_H__
+
+#include <stm32f4xx_hal_uart.h>
 #include "usart.h"
 #include <stdlib.h>
-#include <stdint.h>
 
-#define SLAVE_NAME_LEN          20
-#define MASTER_NAME_LEN         20
-#define SLAVE_NUM               2
-#define TX_SIZE                 140
-#define RX_SIZE                 140
-#define ACK_SIZE                5
+#define RS485_LEN       136
+#define SLAVE_NAME_LEN  20
+#define MASTER_NAME_LEN 20
+#define NUM_SLAVE       2
+#define ID_MASTER       0
+#define ID_SLAVE(x)     (x + 10)     
+#define TX_SIZE         200
+#define RX_SIZE         1028
+#define ACK_SIZE        10        
+#define FRAME_SIZE      136   
 
+// define ACK and NAK value
+#define ACK             0x01
+#define NAK             0xff
 
 struct slave_t
 {
-    char name[SLAVE_NAME_LEN];
     int id;
-};
 
-struct master_t
+    unsigned char name[SLAVE_NAME_LEN];
+};
+struct master_t 
 {
-    char name[MASTER_NAME_LEN];
     int id;
+    unsigned char name[MASTER_NAME_LEN];
 };
-
-enum rs485_error
-{
-    NO_ERROR,
-    SEND_ERROR,
-    RECV_ERROR,
-    ACK_NAK_NOT_SEND,
-    ACK_NAK_NOT_RECV,
-}
 
 struct rs485_t 
 {
     UART_HandleTypeDef * huart;
 
-    struct slave_t slave[SLAVE_NUM];
     struct master_t master;
 
-    char *txbuf;
+    struct slave_t slave[NUM_SLAVE];
+
+    unsigned char *tx_buf;
+    int tx_cnt;
     int tx_size;
-    
-    char *rxbuf;
-    int rxsize;
+    int tx_flag;
+
+    unsigned char *rx_buf;
     int rx_cnt;
+    int rx_size;
+    int rx_flag;
 
-    int tx_ack_size;
-    int rx_ack_size;
-
-    rs485_error status;
+    unsigned char *ack_buf;
+    int ack_cnt;
+    int ack_size;
+    int ack_flag;
 };
 
-rs485_error rs485_init(UART_HandleTypeDef h); 
-rs485_error rs485_transfer(struct rs485_t * handler);
+HAL_StatusTypeDef rs485_init(struct rs485_t * handler);
+HAL_StatusTypeDef rs485_send(struct rs485_t * handler, unsigned char * data, int data_len, int select);
+HAL_StatusTypeDef rs485_recv(struct rs485_t * handler, unsigned char * data, int data_len, int select);
+
+#endif /*__RS485_H__*/
